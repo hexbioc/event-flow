2e5b695da60c7bfb6951dfda8a4be4f52e4c86d2
hexbioc <vaibhav.nayak@gmail.com>
2025-07-15 05:03:48 +0400

    Add sample tests to processor

--- sources/processor/src/message.test.ts
+++
@@ -1,7 +0,0 @@
-import message from "./message";
-
-describe("getMessage()", () => {
-	it("should return the correct message when called", () => {
-		expect(message.get()).toBe("Hello there!");
-	});
-});
